69f13c6cc15f2c17e498cc471648992bbb8f672d
CorentinLouis <corentin.louis@dias.ie>
2022-07-12 10:31:08 +0200

    Removing the writing of the features names on the plot

--- spacelabel/views/matplotlib.py
+++ spacelabel/views/matplotlib.py
@@ -242,8 +242,8 @@ class ViewMatPlotLib(View):
         :param time:
         :param frequency:
         """
-        time_mean = astropyTime(numpy.mean(time.value),format='jd').datetime64
-        frequency_mean = numpy.mean(frequency)
+        #time_mean = astropyTime(numpy.mean(time.value),format='jd').datetime64
+        #frequency_mean = numpy.mean(frequency)
         time_datetime = time.datetime64
 
         for axis in self._ax_data.values():
@@ -253,7 +253,7 @@ class ViewMatPlotLib(View):
                 linestyle='--', linewidth=1.5,
                 alpha=0.75, fill=False
             )
-            axis.text(time_mean, frequency_mean, name)
+            #axis.text(time_mean, frequency_mean, name)
 
     def _create_polyselector(self):
         """
