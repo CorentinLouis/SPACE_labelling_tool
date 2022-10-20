d042e18f2dde0615934ad7f596f1ee47e52d3da7
CorentinLouis <corentin.louis@dias.ie>
2022-10-20 12:48:39 +0200

    Updating the setup.py for the PyPi package

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from distutils.core import setup
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='SPACE Labelling',
-    version='2.0',
-    description='Radio measurement labelling utility',
-    author='Corentin Louis',
+    name='space labelling tool',
+    version='2.0.0',
+    description='Interactive python tool designed to label radio emission features of interest in a temporal-spectral map',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CorentinLouis/SPACE_labelling_tool",
+    authors=[
+    "Corentin K. Louis, C. M. Jackman, S. W. Mangham, K. D. Smith, E. P. O'Dwyer, A. Empey, B. Cecconi, A. Boudouma, P. Zarka, S. Maloney"
+    ],
     maintainer='Corentin Louis',
     maintainer_email='corentin.louis@dias.ie',
     packages=find_packages(),
@@ -20,6 +28,13 @@ setup(
         'scipy',
         'astropy',
         'h5py',
-        'tfcat @ git+https://gitlab.obspm.fr/maser/catalogues/tfcat.git'
-    ]
+        'TFCat'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+    ],
+    python_require='>3.7',
+    license='MIT',
 )
